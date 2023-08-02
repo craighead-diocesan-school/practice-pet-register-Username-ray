b28da2035b2df2316ce1e3f8c9ff2d150d6cbb91
Username-ray <maedara96@craighead.school.nz>
2023-08-02 15:21:23 +1200

    fixed codes and added comments

--- script.js
+++ script.js
@@ -7,62 +7,66 @@ let petsArray = [
     {name : 'Ribon', age : 29, spieces : 'owl'},
     {name : 'Louis', age : 15, spieces : 'koala'},
     {name : 'Michael', age : 120, spieces : 'djungarianHamster'},
-    {name : 'Paradox', age : 2, spieces : elephant},
-    {name : 'Zeus', age : 10, spieces : frog},
+    {name : 'Paradox', age : 2, spieces : 'frog'},
+    {name : 'Zeus', age : 10, spieces : 'frog'},
 ]
+// 
 
 function addingPets () {
+  // get details of new pet
     let name = prompt(" What's your pet's name? ")
     let species = prompt("What spieces does it belong to?")
     let age = prompt('How old is your pet?')
+
+    // create a pet object with the details.
     let pet = {
         name: name,
         age: age,
-        species: species,
+        species: species
     }
-    PermissionStatus.push(pet)
+    
+    // add pet object to pets array
+    petsArray.push(pet)
 }
 
 function showingPets (){
-    alert (petsArray)
+  for(let pet of petsArray){
+    alert (pet.name)
+  }
 }
 
 function removingPets() { 
-    //Keep asking which subjects they want to delete until told to stop.
-    let deletingPet = prompt('Which pet do you want to delete?')
-    while(deletingPet != 'stop'){
-      pet.splice(index, 1)
+    // output the names of each pet
+    let index = 0
+    for(let pet of deletingPet) {
+      alert(index + ': ' + pet.name)
+      index = index + 1
     }
+
+    // get the index of the oet to remove
+    let deletingPet = prompt('Which pet do you want to delete?') 
+
+    // remove the pet at the index
+    pets.splice(deletingPet, 1)
 }
 
 function searchingPets() {
-    let searchingName = prompt('What is the name of pet do you want to search?')
+    let searchingName = prompt('What is the name of pet do you want to find?')
+
+    // create a valiable to store whether we found it or not (haven't yet)
     let searchingResult = false
-    for (let name of petsArray) {
-      if (name == searchingName) {
+
+    // check each pet's name to see if it's the pet we're looking for, update the result if it is
+    for (let pet of petsArray) {
+      if (pet.name == searchingName) {
         searchingResult = true 
       }
     }
   
+    // output whether the search found the pet or not
     if (searchingResult == true){
       alert(searchingName + ' was found! ')
     } else{
       alert(searchingName + ' was not found...')
-    }
-}
-
-function searchingSpecies() {
-    let searchingSpecies = prompt('What is the species of pet do you want to search?')
-    let searchingResult = false
-    for (let name of petsArray) {
-      if (name == searchingSpecies) {
-        searchingResult = true 
-      }
-    }
-  
-    if (searchingResult == true){
-      alert(searchingSpecies + ' was found! ')
-    } else{
-      alert(searchingSpecies + ' was not found...')
     }
 }
